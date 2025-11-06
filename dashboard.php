21b30523397d254cdaa7b844300e3ab242432c22
Heriyanto56 <Ariandasitepu56@gmail.com>
2025-11-06 11:32:52 +0700

    penjualan

--- dashboard.php
+++ dashboard.php
@@ -24,3 +24,12 @@ exit;
 </body>
 
 </html>
+
+// Commit 5 – Setup Awal
+echo "<h2>--POLGAN MART--</h2>";
+echo "<p>Selamat datang, " . $_SESSION['username'] . "!</p><hr>";
+
+// Data produk (array)
+$kode_barang = ["B001", "B002", "B003", "B004", "B005"];
+$nama_barang = ["coca-cola", "Sukro", "Kacang Dua Kelinci", "Floridina", "Lays"];
+$harga_barang = [5000, 12000, 8000, 7000, 15000];
